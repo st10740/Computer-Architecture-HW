f08d65a075703c2c96751eaafd1416f111eeea92
st10740 <ivywang2015@gmail.com>
2023-10-10 14:58:32 +0800

    feat: add c code and assembly code version 1

---
+++ linear_convolution_using_bfloat16.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+float fp32_to_bf16(float x) {
+    float y = x;
+    int *p = (int *) &y;
+    unsigned int exp = *p & 0x7F800000;
+    unsigned int man = *p & 0x007FFFFF;
+    if (exp == 0 && man == 0) /* zero */
+        return x;
+    if (exp == 0x7F800000) /* infinity or NaN */
+        return x;
+
+    /* Normalized number */
+    /* round to nearest */
+    float r = x;
+    int *pr = (int *) &r;
+    *pr &= 0xFF800000;  /* r has the same exp as x */
+    r /= 0x100;
+    y = x + r;
+
+    *p &= 0xFFFF0000;
+
+    return y;
+}
+
+void fp32_to_bf16_in_arr(float* arr, int len) {
+    while(len) {
+        *arr = fp32_to_bf16(*arr);
+        arr += 1;
+        len--;
+    }
+}
+
+void printResult(float* signal, int len) {
+    for(int i=0; i<len; i++) {
+        printf("%f ", *(signal + i));
+    }
+    printf("\n");
+}
+
+float* convolution_with_bf16(float* signal, float* impulse, int sLen, int impluseLen) {
+    int l = sLen + impluseLen -1;
+    float* output = (float*) malloc(sizeof(float)*l);
+
+    /* convert signal from FP32 format to BF16 format */
+    // fp32_to_bf16_in_arr(signal, sLen);
+    // fp32_to_bf16_in_arr(impulse, impluseLen);
+
+    /* do linear convolution */
+    for(int n=0; n<l; n++) {
+        output[n] = 0.0;
+        for(int k=0; k<sLen; k++) {
+            if((n-k) >= 0 && (n-k) <= impluseLen) {
+                output[n] +=  signal[k] * impulse[n-k]; 
+            }
+        }
+    }
+
+    return output;
+}
+
+int main() {
+    float signal1[3] = {1.2, 1.3, 1.4};
+    float impulse1[3] = {2.1, 3.2, 2.1};
+
+    float* result1 = convolution_with_bf16(signal1, impulse1, 3, 3);
+    printResult(result1, 3+3-1);
+    
+    return 0;
+}
---
+++ linear_convolution_using_bfloat16_v1.s
@@ -0,0 +1,102 @@
+.data
+    test1: .word 0x00000000     # exp=0 & man=0
+    test2: .word 0x7F800000     # exp=0x7F800000
+    test3: .word 0x700AB000     # normalized positive number
+    test4: .word 0xF00AB000     # normalized negative number
+    test5: .word 0x707F8FFF     # normalized number with overflow after rounding to nearest
+    exp_mask: .word 0x7F800000
+    man_mask: .word 0x007FFFFF
+    round: .word 8000
+    bfloat16_man_plus_one_mask: .word 0x007F8000
+    bfloat16_mask: .word 0xFFFF0000
+    msg_before_str: .string "before convert to bfloat16: "
+    msg_after_str: .string "after convert to bfloat16: "
+    end_str: .string "\n"
+
+.text
+main:
+    lw a0, test1
+    jal do_fp32_to_bf16_and_print
+    lw a0, test2
+    jal do_fp32_to_bf16_and_print
+    lw a0, test3
+    jal do_fp32_to_bf16_and_print
+    lw a0, test4
+    jal do_fp32_to_bf16_and_print
+    lw a0, test5
+    jal do_fp32_to_bf16_and_print
+    
+    li a7, 10                         # exit
+    ecall
+    
+do_fp32_to_bf16_and_print:
+    addi sp, sp, -8
+    sw ra, 0(sp)
+    sw a0, 4(sp)
+    
+    la a0, msg_before_str
+    li a7, 4
+    ecall                             # print "before convert to bfloat16: " 
+    
+    lw a0, 4(sp)
+    li a7, 2
+    ecall                             # print FP32 format
+    
+    la a0, end_str
+    li a7, 4
+    ecall                             # print "\n"
+    
+    la a0, msg_after_str
+    li a7, 4
+    ecall                             # print "after convert to bfloat16: " 
+    
+    lw a0, 4(sp)
+    jal fp32_to_bf16
+    li a7, 2
+    ecall                             # print BF16 format
+    
+    la a0, end_str
+    li a7, 4
+    ecall                             # print "\n"
+    
+    lw ra, 0(sp)
+    lw a0, 4(sp)
+    addi sp, sp, 8
+    jr ra
+
+fp32_to_bf16:
+    addi sp, sp, -4
+    sw ra, 0(sp)
+    add t0, a0, zero                 # store a0 to t0
+    lw t1, exp_mask                  
+    and t2, t0, t1                   # t2: result of applying exp mask to t0
+    beq t1, t2, return_fp32_to_bf16  # t0 is infinity or NaN, just return it
+    beq t2, zero, exp_is_zero
+    
+normalized_number: 
+    lw t1, bfloat16_man_plus_one_mask 
+    lw t2, round                   
+    and t3, t0, t1                    # t3: applying bfloat16_man_plus_one_mask to t0
+    beq t1, t3, is_overflow           # doing round to nearest will cause overflow
+    
+    add t0, t0, t2                    # do round to nearest
+    lw t1, bfloat16_mask           
+    and a0, t0, t1                    # a0: applying bfloat16_mask to t0
+    
+return_fp32_to_bf16:
+    lw ra, 0(sp)
+    addi sp, sp, 4
+    jr ra
+
+exp_is_zero:
+    lw t1, man_mask                
+    and t1, t1, t0                    # t1: applying man_mask to t0
+    bne t1, zero, normalized_number   # if t1!=0, t0 is normalized number
+    j return_fp32_to_bf16             # else, t0 is zero, just return it
+
+# if mantissa & the most left bit of its right bits are all 1
+# doing round to nearest will cause overflow, so I do round down instead 
+is_overflow:
+    lw t1, bfloat16_mask
+    and a0, t0, t1                    # do round down
+    j return_fp32_to_bf16
